a79ff32e23019302a2ae7dd593c6d657591c9e48
Lucas <lucasrm90@outlook.com>
2023-04-18 14:32:01 -0300

    Update server.py

--- server.py
+++ server.py
@@ -9,13 +9,13 @@ from http import HTTPStatus
 class Handler(http.server.SimpleHTTPRequestHandler):
     def do_GET(self):
         self.send_response(HTTPStatus.OK)
-        self.end_headers()
-        time.sleep(10)
-        msg = 'Hello! you requested %s' % (self.path)
+        self.end_headers()        
+        time.sleep(2400)
+        msg = 'Concluido! feito chamada para API 40 minutos de espera %s' % (self.path)
         self.wfile.write(msg.encode())
 
 
-port = int(os.getenv('PORT', 80))
+port = int(os.getenv('PORT', 8089))
 print('Listening on port %s' % (port))
 httpd = socketserver.TCPServer(('', port), Handler)
 httpd.serve_forever()
